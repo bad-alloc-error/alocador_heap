c80eabbb386eef9bdb12a92df115a2cda398938c
Filipe Cruz <bad-alloc-error@yandex.com>
2022-07-29 21:44:04 -0300

    initial commit

--- headers/mmanager.h
+++ headers/mmanager.h
@@ -1,7 +1,7 @@
 #ifndef _MMANAGER_H_
 #define _MMANAGER_H_
 
-void mmanager_init();
+void mmanager_init(void);
 static void* mmanager_page_alloc(int vmp_units);
 static signed int mmanager_page_dealloc(void* memory_page_addr, int units);
 
