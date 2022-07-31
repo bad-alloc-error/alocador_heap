ad9ccc0d41562fba76078688bca52c43c3e56d39
Filipe Cruz <bad-alloc-error@yandex.com>
2022-07-30 23:41:07 -0300

    descrição da macro MAXSIZE_PAGE_FAMILY_NAME

--- headers/mmanager.h
+++ headers/mmanager.h
@@ -1,6 +1,12 @@
 #ifndef _MMANAGER_H_
 #define _MMANAGER_H_
 
+/*Para obter o número máximo de page_family_t que podemos armazenar dentro de uma page
+    do tipo vm_page_for_families_t é pegar o tamanho da pagina alocada, subtrair pelo tamanho
+    do ponteiro do tipo vm_page_for_families_t e dividir pelo tamanho da objeto vm_page_family_t.
+    Na base da página (vm_page_for_families_t) tera um campo onde armazeno o endereço 
+    da proxima vm_page_for_families_t e a partir dai as vm_page_family_t serão empilhadas no espaço
+    remanescente da página. Lembrando que a heap cresce pra cima.*/
 #define MAX_FAMILIES_PER_VM_PAGE \
     (PAGE_SIZE - sizeof(vm_page_for_families_t *)/\
         sizeof(vm_page_family_t))
