442da54137598ffea649fdd0b4d5eb3f9918c8d9
Filipe Cruz <bad-alloc-error@yandex.com>
2022-08-01 03:20:37 -0300

    page registration implementation

--- headers/mmanager.h
+++ headers/mmanager.h
@@ -6,10 +6,21 @@
     do ponteiro do tipo vm_page_for_families_t e dividir pelo tamanho da objeto vm_page_family_t.
     Na base da página (vm_page_for_families_t) tera um campo onde armazeno o endereço 
     da proxima vm_page_for_families_t e a partir dai as vm_page_family_t serão empilhadas no espaço
-    remanescente da página. Lembrando que a heap cresce pra cima.*/
+    remanescente da página. */
+
 #define MAX_FAMILIES_PER_VM_PAGE \
     (PAGE_SIZE - sizeof(vm_page_for_families_t *)/\
         sizeof(vm_page_family_t))
+
+#define ITER_PAGE_FAMILY_BEGIN(first_page_for_families_ptr, current) \
+{                                                          \
+    __uint32_t count = 0;                                  \
+    for(current = (vm_page_family_t *)&first_page_for_families_ptr->vm_page_family[0]; \
+                    current->size && count < MAX_FAMILIES_PER_VM_PAGE; current++, count++){ \
+
+#define ITER_PAGE_FAMILY_END(first_page_for_families_ptr, current) }}
+
+#define MAXSIZE_PAGE_FAMILY_NAME 32
 
 typedef struct vm_page_family_{
     char struct_name[MAXSIZE_PAGE_FAMILY_NAME];
@@ -17,7 +28,7 @@ typedef struct vm_page_family_{
 }vm_page_family_t;
 
 typedef struct vm_page_for_families_{
-    struct vm_page_for_families_t next*;
+    struct vm_page_for_families_t *next;
     vm_page_family_t vm_page_family[0];
 }vm_page_for_families_t;
 
@@ -25,5 +36,6 @@ typedef struct vm_page_for_families_{
 void mmanager_init(void);
 static void* mmanager_page_alloc(int vmp_units);
 static signed int mmanager_page_dealloc(void* memory_page_addr, int units);
+void mmanager_new_page_family(char* stuct_name, __uint32_t size);
 
 #endif
--- mmanager.c
+++ mmanager.c
@@ -1,9 +1,29 @@
 #include<unistd.h>
 #include<memory.h>
 #include<sys/mman.h>
+#include<string.h>
 #include"headers/mmanager.h"
 
-static size_t PAGE_SIZE = {0};
+static size_t PAGE_SIZE = 0;
+
+/* Aponta para o objeto instanciado mais recentemente.
+    
+    1- Instancia uma nova vm_page_family_t.
+
+    2- Se o gerenciador de memória ainda não tiver nenhuma 
+    página virtual alocada (vm_page_for_families_t), então
+    ele requer uma página para o kernel e atualiza a variavel
+    first_vm_page_for_families apontando para um página válida.
+
+    3- Verifica se a nova vm_page_family_t pode ser adicionada
+        a first_vm_page_for_families_t.
+        3.1- Se caso ela pode ser adicionada, então vm_page_family_t é
+             adicionada no array vm_page_family da estrutura vm_page_for_families_t.
+             3.2- No caso de não entrada então o gerenciador requer uma nova
+             página para o kernel, atualiza o ponteiro first_vm_page_for_families
+             para a página alocada mais recentemente.
+*/
+static vm_page_for_families_t *first_vm_page_for_families = NULL;
 
 void mmanager_init(){
 
@@ -39,4 +59,52 @@ static signed int mmanager_page_dealloc(void* memory_page_addr, int units){
 
     return SUCCESS_RELEASE;
 
+}
+
+void mmanager_new_page_family(char* struct_name, __uint32_t size){
+
+    vm_page_family_t* vm_page_family_curr = NULL;
+    vm_page_for_families_t* new_vm_page_for_families = NULL;
+
+    if(size > PAGE_SIZE){
+
+        printf("Error: %s() structure %s size exceeds system page size\n", __FUNCTION__, struct_name);
+        return;
+    }
+
+    if(!first_vm_page_for_families){
+
+        /* Caso nulo, registra uma nova vm_page_for_families_t (tam: PAGE_SIZE)*/
+        first_vm_page_for_families = (vm_page_for_families_t *) mmanager_page_alloc(1);
+        first_vm_page_for_families->next = NULL;
+
+        /* Popula a struct */
+        strncpy(first_vm_page_for_families->vm_page_family[0].struct_name, struct_name, MAXSIZE_PAGE_FAMILY_NAME);
+        first_vm_page_for_families->vm_page_family[0].size = size;
+
+        return;
+    }
+
+    __uint32_t count = 0;
+
+    ITER_PAGE_FAMILY_BEGIN(first_vm_page_for_families, vm_page_family_curr){
+
+        if(strncmp(vm_page_family_curr->struct_name, struct_name, MAXSIZE_PAGE_FAMILY_NAME) != 0){
+            count++;
+            continue;
+        }
+
+    }ITER_PAGE_FAMILY_END(first_vm_page_for_families, vm_page_family_curr);
+
+    if(count == MAX_FAMILIES_PER_VM_PAGE){
+
+        new_vm_page_for_families = (vm_page_for_families_t *)mmanager_page_alloc(1);
+        new_vm_page_for_families->next = first_vm_page_for_families;
+        first_vm_page_for_families = new_vm_page_for_families;
+        vm_page_family_curr = &first_vm_page_for_families->vm_page_family[0];
+    }
+
+    strncpy(vm_page_family_curr->struct_name, struct_name, MAXSIZE_PAGE_FAMILY_NAME);
+    vm_page_family_curr->size = size;
+    
 }
