93d1b72abcf7b7fdf43e594dea813c5a4950e891
Filipe Cruz <bad-alloc-error@yandex.com>
2022-08-08 17:01:31 -0300

    removido duas declarações da mesma função

--- headers/mmanager_api.h
+++ headers/mmanager_api.h
@@ -5,9 +5,8 @@
 /*Interface entre o Heap Manager Lib e aplicação do usuário*/
 
 void mmanager_init(void);
-vm_page_family_t* lookup_page_family_by_name (char *struct_name);
-void mmanager_new_page_family(char* struct_name, uint32_t struct_size);
 void mmanager_print_registered_page_families();
+vm_page_family_t* lookup_page_family_by_name (char *struct_name);
 
 #define MMANAGER_STRUCT_REGISTER(STRUCT_NAME) \
     (mmanager_new_page_family(#STRUCT_NAME, sizeof(STRUCT_NAME)))
