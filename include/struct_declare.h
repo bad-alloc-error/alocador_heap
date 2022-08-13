e822329296768d06253bbeb92f1d3aaaf19e45b5
Filipe Cruz <bad-alloc-error@yandex.com>
2022-08-13 17:50:42 -0300

    declaração e documentaçao das estruturas

---
+++ include/struct_declare.h
@@ -0,0 +1,12 @@
+#ifndef __STRUCT_DECLARE_H__
+#define __STRUCT_DECLARE_H__
+
+/*DECLARAÇÃO DAS ESTRUTURAS DE DADOS E SUA DOCUMENTAÇÃO*/
+
+typedef struct vm_page_t vm_page_t;
+typedef struct vm_page_family_t vm_page_family_t;
+typedef struct vm_page_for_families_t vm_page_for_families_t;
+typedef struct vm_meta_block_data_t vm_meta_block_data_t;
+
+
+#endif
